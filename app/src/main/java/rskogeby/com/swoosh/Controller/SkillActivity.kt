dccd485ffaacdf7d136286b5c0a7fa12f10331f2
Richard Skogeby <36446048+rSkogeby@users.noreply.github.com>
2018-03-05 18:20:56 +0100

    Errors fixed in skill

--- app/src/main/java/rskogeby/com/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/rskogeby/com/swoosh/Controller/SkillActivity.kt
@@ -1,6 +1,10 @@
 package rskogeby.com.swoosh.Controller
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_skill.*
 import rskogeby.com.swoosh.Utilities.EXTRA_LEAGUE
 import rskogeby.com.swoosh.R
 
@@ -11,5 +15,27 @@ class SkillActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         league = intent.getStringExtra(EXTRA_LEAGUE)
+    }
+    fun onBallerClick(view: View) {
+        beginnerBtn.isChecked = false
+        league="baller"
+    }
+
+    fun onBeginnerClick(view: View) {
+        ballerBtn.isChecked = false
+        league = "beginner"
+    }
+
+    fun isToggled(): Boolean {
+        return ballerBtn.isChecked || beginnerBtn.isChecked
+    }
+
+    fun onSkillFinishClick(view: View) {
+        if (league != "" && isToggled()) {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this,"Please select a skill level.",Toast.LENGTH_SHORT).show()
+        }
     }
 }
